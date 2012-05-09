907bbdcc4ba893f8b6e8cbb0f05735309ba563a3
Anders A. Søndergaard <andersas@gmail.com>
2012-05-09 16:01:06 +0200

    More structure..

--- optimization/simulated_annealing.c
+++ optimization/simulated_annealing.c
@@ -2,6 +2,7 @@
 #include "../linalg/linalg.h"
 #include <gsl/gsl_rng.h>
 #include <pthread.h>
+#include <string.h>
 #include <math.h>
 
 
@@ -42,34 +43,56 @@ void annealing_step(size_t dim, double *x, double step_size, gsl_rng *rng) {
 }
 
 
-void simulated_annealing(size_t dimension, double *result, \
+double simulated_annealing(size_t dimension, double *result, \
 			          double (*f)(const double x[dimension]), \ 
 					const double lower_limits[], \
 					const double higher_limits[], \
 					double max_step_distance, \
-					int n_runs, double temperature, double dT) {
+					int n_runs, double temperature) {
 
 	
 	double T;
+	double x[dimension];
+	double z[dimension];
 	double bestx[dimension];
 	double bestf;
+	double fx,fz;
 	unsigned long int seed;
+	int step;
 	gsl_rng *rnd = gsl_rng_alloc(gsl_rng_mt19937);
 	FILE *handle = fopen("/dev/urandom", "r");
 
 	fread(&seed, sizeof(unsigned long int), 1, handle);
 	gsl_rng_set(rnd,seed);
 
-	T = temperature;
-	while (T > 0) {
 
+	get_initial_point(dimension,x,lower_limits,higher_limits,rng);
+	
+	memcpy(z,x,sizeof(double)*dimension);
+	memcpy(bestx,x,sizeof(double)*dimension);
+	bestf = f(x);
+	fx = bestf;
+
+	step = 0;
+	do {
 		
-		   t = gamma/log(k+2), k = number of steps,
-			gamme problem constant
+		annealing_step(dimension, z, max_step_size, rng);
 
-			probabilities = min(1,exp(f(next)-f(now)/T));
+		fz = f(z);
 
-	}
+		if (fz <= fx) {
+			
+		} else {
+
+		}
+		
+		
+		//   t = gamma/log(k+2), k = number of steps,
+		//	gamme problem constant
+
+		//	probabilities = min(1,exp(f(next)-f(now)/T));
+
+	} while();
 
 
 	fclose(handle);
